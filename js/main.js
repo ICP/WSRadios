1e46f2d7df72f77d582e3142ce1f0ade2e0c3211
Farid Rn <faridv@gmail.com>
2015-08-25 19:25:16 +0430

    Working on homepage

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".lang-select").on('click', function (e) {
+	if ($("#languages").is(':visible'))
+	    $("#languages").slideUp();
+	else
+	    $("#languages").slideDown(function () {
+		$("html, body").animate({scrollTop: $("#languages").offset().top});
+	    });
+	e.preventDefault();
+    });
+});
