276d721eb7a962c799b5058d31c534ceb2776500
Farid Rn <faridv@gmail.com>
2015-08-31 19:12:02 +0430

    Working on gallery page

---
+++ gallery.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>{Title}</title>
+		<meta name="description" content="{Description}">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<?php include(dirname(__FILE__) . '/blocks/header.php'); ?>
+		<section id="main">
+			<div class="wrapper _light-blue _border-top _border-bottom">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-8">
+							<div class="panel ticker">
+								<div class="panel-title">Breaking News</div>
+								<div class="panel-body">
+									<ul class="items">
+										<li><a href="#">Burundi counting votes after presidential election</a></li>
+										<li><a href="#">Greek lawmakers to vote on 2nd bailout reform bill</a></li>
+										<li><a href="#">FIFA, Laos to probe reports of trafficking African teen footballers</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4 hidden-xs">
+							<div class="date text-right">
+								Wednesday, 22 July 2015 
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="main-container" class="wrapper _white">
+				<div class="container">
+					<div class="row">
+						<div id="mainbody" class="col-xs-12 col-md-9">
+							<div id="gallery">
+								<div class="panel gallery">
+									<div class="panel-heading">
+										<h2>Gallery</h2>
+									</div>
+									<div class="panel-body">
+										<div class="items">
+											<ul class="list-unstyled">
+												<li>
+													<figure>
+														<img src="data/smpl1.jpg" alt="" />
+														<figcaption class="desc">
+															<h3>A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die</h3>
+															<div class="date">Sunday, 30 August 2015 15:13</div>
+															<p>Air strikes rattled the city of Snizhne in eastern Ukraine Tuesday. Pro-Russia rebels blamed Kiev, and some in Kiev seemed to blame Russia.</p>
+														</figcaption>
+													</figure>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="panel-footer">
+										<div class="sharings">
+											<ul class="list-unstyled list-inline">
+												<li class="blue-dark"><a href="https://www.facebook.com/sharer/sharer.php?u={{url}}" target="_blank"><i class="icon-facebook"></i></a></li>
+												<li class="blue-light"><a href="https://twitter.com/home?status={{url}}" target="_blank"><i class="icon-twitter"></i></a></li>
+												<li class="red"><a href="https://plus.google.com/share?url{{url}}" target="_blank"><i class="icon-gplus"></i></a></li>
+												<li class="red-dark"><a href="#" target="_blank"><i class="icon-pinterest"></i></a></li>
+												<li class="blue-light2"><a href="#" target="_blank"><i class="icon-linkedin"></i></a></li>
+												<li class="orange"><a href="#" target="_blank"><i class="icon-blogger"></i></a></li>
+												<li class="black"><a href="#" target="_blank"><i class="icon-digg"></i></a></li>
+												<li class="gray"><a href="#"><i class="icon-mail"></i></a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="panel tiles title-pattern">
+									<h3 class="panel-title">
+										<a href="#">More from Galleries</a>
+									</h3>
+									<div class="panel-body">
+										<div class="items">
+											<ul class="list-unstyled list-inline">
+												<li>
+													<figure>
+														<a class="img" href="#">
+															<img src="data/smpl1.jpg" alt="" />
+														</a>
+														<figcaption>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die<span class="meta">(16 photos)</span></a>
+														</figcaption>
+													</figure>
+												</li>
+												<li>
+													<figure>
+														<a class="img" href="#">
+															<img src="data/smpl1.jpg" alt="" />
+														</a>
+														<figcaption>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die<span class="meta">(16 photos)</span></a>
+														</figcaption>
+													</figure>
+												</li>
+												<li>
+													<figure>
+														<a class="img" href="#">
+															<img src="data/smpl1.jpg" alt="" />
+														</a>
+														<figcaption>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die<span class="meta">(16 photos)</span></a>
+														</figcaption>
+													</figure>
+												</li>
+												<li>
+													<figure>
+														<a class="img" href="#">
+															<img src="data/smpl1.jpg" alt="" />
+														</a>
+														<figcaption>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die<span class="meta">(16 photos)</span></a>
+														</figcaption>
+													</figure>
+												</li>
+												<li>
+													<figure>
+														<a class="img" href="#">
+															<img src="data/smpl1.jpg" alt="" />
+														</a>
+														<figcaption>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die<span class="meta">(16 photos)</span></a>
+														</figcaption>
+													</figure>
+												</li>
+												<li>
+													<figure>
+														<a class="img" href="#">
+															<img src="data/smpl1.jpg" alt="" />
+														</a>
+														<figcaption>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die<span class="meta">(16 photos)</span></a>
+														</figcaption>
+													</figure>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<?php include(dirname(__FILE__) . '/blocks/sidebar.php'); ?>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php include(dirname(__FILE__) . '/blocks/footer.php'); ?>
+	</body>
+</html>
