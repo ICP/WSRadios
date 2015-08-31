62cc27aab61c4ef9d2dfe9dfb259356d39689a67
Farid Rn <faridv@gmail.com>
2015-08-31 16:48:36 +0430

    Working on itemlist page

---
+++ itemlist.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>{Title}</title>
+		<meta name="description" content="{Description}">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<?php include(dirname(__FILE__) . '/blocks/header.php'); ?>
+		<section id="main">
+			<div class="wrapper _light-blue _border-top _border-bottom">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-8">
+							<div class="panel ticker">
+								<div class="panel-title">Breaking News</div>
+								<div class="panel-body">
+									<ul class="items">
+										<li><a href="#">Burundi counting votes after presidential election</a></li>
+										<li><a href="#">Greek lawmakers to vote on 2nd bailout reform bill</a></li>
+										<li><a href="#">FIFA, Laos to probe reports of trafficking African teen footballers</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4 hidden-xs">
+							<div class="date text-right">
+								Wednesday, 22 July 2015 
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="main-container" class="wrapper _white">
+				<div class="container">
+					<div class="row">
+						<div id="mainbody" class="col-xs-12 col-md-9">
+							<div id="itemlist" >
+								<div class="panel itemlist news vertical">
+									<div class="panel-heading">
+										<h3><a href="#">Europe</a></h3>
+									</div>
+									<div class="panel-body items">
+										<ul class="list-unstyled">
+											<li class="featured">
+												<figure>
+													<a class="img" href="#">
+														<img src="data/smpl1.jpg" alt="" />
+													</a>
+													<figcaption class="desc">
+														<h3>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die</a>
+														</h3>
+														<div class="date">Sunday, 30 August 2015 15:13</div>
+														<p>Air strikes rattled the city of Snizhne in eastern Ukraine Tuesday. Pro-Russia rebels blamed Kiev, and some in Kiev seemed to blame Russia.</p>
+													</figcaption>
+												</figure>
+											</li>
+											<li>
+												<figure>
+													<a class="img" href="#">
+														<img src="data/smpl1.jpg" alt="" />
+													</a>
+													<figcaption>
+														<h3>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die</a>
+														</h3>
+														<div class="date">Sunday, 30 August 2015 15:13</div>
+														<p>Air strikes rattled the city of Snizhne in eastern Ukraine Tuesday. Pro-Russia rebels blamed Kiev, and some in Kiev seemed to blame Russia.</p>
+													</figcaption>
+												</figure>
+											</li>
+											<li>
+												<figure>
+													<a class="img" href="#">
+														<img src="data/smpl1.jpg" alt="" />
+													</a>
+													<figcaption>
+														<h3>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die</a>
+														</h3>
+														<div class="date">Sunday, 30 August 2015 15:13</div>
+														<p>Air strikes rattled the city of Snizhne in eastern Ukraine Tuesday. Pro-Russia rebels blamed Kiev, and some in Kiev seemed to blame Russia.</p>
+													</figcaption>
+												</figure>
+											</li>
+											<li>
+												<figure>
+													<a class="img" href="#">
+														<img src="data/smpl1.jpg" alt="" />
+													</a>
+													<figcaption>
+														<h3>
+															<a href="#">A Bomb Seemingly Misses Its Mark in Ukraine, and 11 More Civilians Die</a>
+														</h3>
+														<div class="date">Sunday, 30 August 2015 15:13</div>
+														<p>Air strikes rattled the city of Snizhne in eastern Ukraine Tuesday. Pro-Russia rebels blamed Kiev, and some in Kiev seemed to blame Russia.</p>
+													</figcaption>
+												</figure>
+											</li>
+										</ul>
+										<div class="text-center">
+											<ul class="pagination">
+												<li>
+													<a href="#" aria-label="Previous">
+														<span aria-hidden="true">&laquo;</span>
+													</a>
+												</li>
+												<li><a href="#">1</a></li>
+												<li><a href="#">2</a></li>
+												<li><a href="#">3</a></li>
+												<li><a href="#">4</a></li>
+												<li><a href="#">5</a></li>
+												<li>
+													<a href="#" aria-label="Next">
+														<span aria-hidden="true">&raquo;</span>
+													</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<?php include(dirname(__FILE__) . '/blocks/sidebar.php'); ?>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php include(dirname(__FILE__) . '/blocks/footer.php'); ?>
+	</body>
+</html>
