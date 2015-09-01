f1ac5c15fc458c5f3cf75dce6acf562d59e249d1
Farid Rn <faridv@gmail.com>
2015-09-01 12:56:04 +0430

    Homepage and itemlist page RTL

--- itemlist.php
+++ itemlist.php
@@ -12,7 +12,7 @@
 		<link rel="stylesheet" href="css/style.css">
 		<script src="js/modernizr-2.6.2.min.js"></script>
 	</head>
-	<body>
+	<body class="rtl">
 		<?php include(dirname(__FILE__) . '/blocks/header.php'); ?>
 		<section id="main">
 			<div class="wrapper _light-blue _border-top _border-bottom">
