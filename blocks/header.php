1f7adfb400e65005e123cbe40c75e57732c36a37
Farid Rn <faridv@gmail.com>
2015-12-26 21:57:16 +0330

    Updated header structure

--- blocks/header.php
+++ blocks/header.php
@@ -1,69 +1,152 @@
 <header id="header">
-			<div class="wrapper _dark-blue">
-				<div class="container">
-					<div id="masthead" class="row">
-						<div class="col-xs-8 col-sm-6 col-md-5">
-							<h1 class="logo">
-								<a href="#">{Title}</a>
-								<span class="lang-title">English</span>
-							</h1>
-						</div>
-						<div class="col-xs-4 col-sm-6 col-md-7">
-							<form class="search" role="form" action="#">
-								<div class="form-group">
-									<input type="text" name="q" class="form-control" placeholder="Search" />
-									<button type="submit"><i class="icon-search"></i></button>
-								</div>
-								<div class="clearfix"></div>
-							</form>
-							<div class="social-icons">
-								<ul class="list-unstyled list-inline text-right">
-									<li><a href="#"><i class="icon-facebook"></i></a></li>
-									<li><a href="#"><i class="icon-twitter"></i></a></li>
-									<li><a href="#"><i class="icon-gplus"></i></a></li>
-									<li><a href="#"><i class="icon-rss"></i></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div id="menu" class="row">
-						<div class="col-xs-12">
-							<nav>
-								<ul class="menu-items list-unstyled list-inline">
-									<li>
-										<a href="#">Home</a>
-									</li>
-									<li class="haschild">
-										<a href="#">News</a>
-										<ul class="child">
-											<li><a href="#">Submenu</a></li>
-											<li><a href="#">Submenu 2</a></li>
-											<li><a href="#">Other Submenu</a></li>
-										</ul>
-									</li>
-									<li class="haschild active">
-										<a href="#">Menu Item</a>
-										<ul class="child">
-											<li><a href="#">Submenu</a></li>
-											<li><a href="#">Submenu 2</a></li>
-											<li><a href="#">Other Submenu</a></li>
-										</ul>
-									</li>
-									<li>
-										<a href="#">Menu Item</a>
-									</li>
-									<li class="haschild">
-										<a href="#">Menu Item</a>
-										<ul class="child">
-											<li><a href="#">Submenu</a></li>
-											<li><a href="#">Submenu 2</a></li>
-											<li><a href="#">Other Submenu</a></li>
-										</ul>
-									</li>
-								</ul>
-							</nav>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
+    <div id="masthead">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-8 col-sm-6 col-md-4">
+                    <h1 class="logo">
+                        <a href="#">Pars Today</a>
+                    </h1>
+                    <div class="lang-select" data-toggle="toggle" data-target="#languages">English</div>
+                </div>
+                <div class="col-xs-4 col-sm-6 col-md-8">
+                    <div class="row">
+                        <div class="col-xs-10 col-md-6">
+                            <ul class="header-menu list-inline list-unstyled">
+                                <li class="active"><a href="#">Home</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><a href="#">Radio</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-10 col-md-6">
+                            <form class="search" role="form" action="#">
+                                <div class="form-group">
+                                    <input type="text" name="q" class="form-control" placeholder="Search" />
+                                    <button type="submit"><i class="icon-search"></i></button>
+                                </div>
+                                <div class="clearfix"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="languages" style="display: none;">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-2">
+                    <ul>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-2">
+                    <ul>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-2">
+                    <ul>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-2">
+                    <ul>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-2">
+                    <ul>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-2">
+                    <ul>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                        <li><a href="#">Language name</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav id="menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <ul class="menu-items list-unstyled list-inline">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="haschild">
+                            <a href="#">News</a>
+                            <ul class="child">
+                                <li><a href="#">Submenu</a></li>
+                                <li><a href="#">Submenu 2</a></li>
+                                <li><a href="#">Other Submenu</a></li>
+                            </ul>
+                        </li>
+                        <li class="haschild active">
+                            <a href="#">Menu Item</a>
+                            <ul class="child">
+                                <li><a href="#">Submenu</a></li>
+                                <li><a href="#">Submenu 2</a></li>
+                                <li><a href="#">Other Submenu</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Menu Item</a>
+                        </li>
+                        <li class="haschild">
+                            <a href="#">Menu Item</a>
+                            <ul class="child">
+                                <li><a href="#">Submenu</a></li>
+                                <li><a href="#">Submenu 2</a></li>
+                                <li><a href="#">Other Submenu</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
