b57ce033d3d5084f538750d8bc2bb928bfe08ae9
Farid Rn <faridv@gmail.com>
2015-12-26 21:56:56 +0330

    Made page more dynamic

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 $(function () {
-    $(".lang-select").on('click', function (e) {
-	if ($("#languages").is(':visible'))
-	    $("#languages").slideUp();
-	else
-	    $("#languages").slideDown(function () {
-		$("html, body").animate({scrollTop: $("#languages").offset().top});
-	    });
-	e.preventDefault();
+    $("[data-toggle]").on('click', function (e) {
+        var $target = $($(this).attr('data-target'));
+        $(this).toggleClass('active');
+        switch ($(this).attr('data-toggle')) {
+            case 'toggle':
+                if ($target.is(":hidden"))
+                    $target.slideDown();
+                else
+                    $target.slideUp();
+                break;
+        }
+        e.preventDefault();
     });
 
     if ($(".panel.slider").length) {
-	var $slider = $(".panel.slider");
-	$.each($slider, function() {
-	    var $this = $(this);
-	    var slider = $this.find(".items ul").owlCarousel({
-		loop: true
-		, items: 4
-		, margin: 0
-		, autoplay: false
-		, responsive: {
-		    0: { items: 2 }
-		    , 480: { items: 4 }
-		}
-	    });
-	    $this.find(".pager .next").click(function(e) {
-		slider.trigger('next.owl.carousel');
-		e.preventDefault();
-	    });
-	    $this.find(".pager .prev").click(function(e) {
-		slider.trigger('prev.owl.carousel');
-		e.preventDefault();
-	    });
-	});
+        var $slider = $(".panel.slider");
+        $.each($slider, function () {
+            var $this = $(this);
+            var slider = $this.find(".items ul").owlCarousel({
+                loop: true
+                , items: 4
+                , margin: 0
+                , autoplay: false
+                , responsive: {
+                    0: {items: 2}
+                    , 480: {items: 4}
+                }
+            });
+            $this.find(".pager .next").click(function (e) {
+                slider.trigger('next.owl.carousel');
+                e.preventDefault();
+            });
+            $this.find(".pager .prev").click(function (e) {
+                slider.trigger('prev.owl.carousel');
+                e.preventDefault();
+            });
+        });
     }
 });
