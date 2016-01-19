8f3d74ad7985f508d14263bd6eb251b8cb3a8235
Farid Rn <faridv@gmail.com>
2016-01-19 19:45:05 +0330

    Added youtube icon

--- blocks/footer.php
+++ blocks/footer.php
@@ -5,7 +5,7 @@
                 <a href="#" class="footer-logo">Pars Today</a>
                 <p class="copyright">© Copyright 2015 ParsToday.<br />All rights reserved.</p>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-7">
+            <div class="col-xs-12 col-sm-6 col-md-6">
                 <div class="row">
                     <div class="col-xs-6 col-md-3">
                         <dl class="footer-menu">
@@ -49,12 +49,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-12 col-md-2">
+            <div class="col-xs-12 col-md-3">
                 <ul class="social-icons">
                     <li><a href="#"><i class="icon-facebook"></i></a></li>
                     <li><a href="#"><i class="icon-twitter"></i></a></li>
                     <li><a href="#"><i class="icon-gplus"></i></a></li>
                     <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                    <li><a href="#"><i class="icon-youtube"></i></a></li>
                 </ul>
             </div>
         </div>
